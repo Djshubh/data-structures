426a2a397cc867b27b32378fdc03166cddef4473
Shubham Kumar <shubhamkmr698@gmail.com>
2016-06-12 21:15:29 +0530

    Linked List - deletion

---
+++ delete_pos_list.cpp
@@ -0,0 +1,90 @@
+// deleting a node from linked list : as position
+
+#include <iostream>
+
+using namespace std;
+
+struct listNode {
+    int data ;
+    listNode *next;
+};
+
+
+void traversal(listNode *current){
+ while(current){
+        cout << current->data <<"  ";
+        current =current->next;
+     }
+ }
+
+listNode *create_list(int dt,listNode *p){
+    listNode *q;
+    q = new listNode;
+    q->data = dt;
+    q->next = p;
+    return q;
+}
+
+void list_delete(int pos , listNode **head){
+
+    listNode *q;
+    q = new listNode;
+
+    int found=0,ctr=1;
+
+    listNode *current;
+    current = new listNode;
+    current = *head;
+
+    if(pos ==1){
+        *head = current->next;
+        found = 1;
+    }
+    while(ctr<pos && found ==0 && current!=NULL){
+
+              q = current ;
+              current = current->next;
+              ctr++;
+        }
+
+    if(current!=NULL){
+        cout << "data deleted : "<< current->data <<endl;
+        q->next = current->next;
+        delete current;
+        found =1;
+    }
+
+    if(found == 1) cout << "node deleted\n ";
+    else           cout << "node not found in the list\n";
+
+}
+
+
+int main(){
+
+    int val, ctr, no;
+    listNode *head;
+    head = new listNode;
+    head = NULL;
+    cout << "\nEnter the list size ";
+    cin >> no;
+    cout << "enter values : ";
+    for(int i = 0; i<no; i++){
+        cin >> val;
+        head = create_list(val,head);
+
+    }
+
+    traversal(head);
+
+    cout << "\nEnter position to be deleted ";
+    cin  >> no;
+
+    list_delete(no, &head);
+
+    cout << "list after deletion " ;
+    traversal(head);
+
+}
+
+
